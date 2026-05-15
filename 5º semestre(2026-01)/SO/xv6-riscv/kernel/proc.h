9f74eb51a9be680c862f7156b1d141eae4262ce1
Luanllp <luan.llp2005@gmail.com>
2026-05-15 01:33:10 -0300

    Adição da classe de prioridade aos processos

--- 5º semestre(2026-01)/SO/xv6-riscv/kernel/proc.h
+++ 5º semestre(2026-01)/SO/xv6-riscv/kernel/proc.h
@@ -91,6 +91,7 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
+  int priority_class;          // Variável para classe de prioridade (0,1,2 ou 3)
 
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
