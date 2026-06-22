40ee3d5380b587937ca05d4b360f93954ed283f3
Luanllp <luan.llp2005@gmail.com>
2026-06-22 19:46:28 -0300

    feat(templates): adiciona botao de navegacao para novo-horario no index.html

--- 5º semestre(2026-01)/top.43/puma/core/views.py
+++ 5º semestre(2026-01)/top.43/puma/core/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import HorarioDisponivel
+from .forms import HorarioDisponivelForm
 
 def home(request):
     data = {}
-    # Buscamos todos os horários cadastrados no banco de dados
     data['horarios'] = HorarioDisponivel.objects.all()
-    
-    # Renderiza a página index.html passando os dados do banco 
     return render(request, 'index.html', data)
+
+def cadastrar_horario(request):
+    if request.method == 'POST':
+        form = HorarioDisponivelForm(request.POST)
+        if form.is_valid():
+            # Salva o formulário provisoriamente para injetar o usuário logado como responsável
+            horario = form.save(commit=False)
+            horario.responsavel = request.user
+            horario.save()
+            return redirect('/') # Redireciona de volta para a página inicial
+    else:
+        form = HorarioDisponivelForm()
+        
+    return render(request, 'cadastrar_horario.html', {'form': form})
