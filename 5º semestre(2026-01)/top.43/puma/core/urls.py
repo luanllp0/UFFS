edc6268c93c18efb1e48094845988caf13649309
Luanllp <luan.llp2005@gmail.com>
2026-06-22 19:51:05 -0300

    feat(core): finaliza CRUD de HorarioDisponivel com funcoes de Update e Delete

--- 5º semestre(2026-01)/top.43/puma/core/urls.py
+++ 5º semestre(2026-01)/top.43/puma/core/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('novo-horario/', views.cadastrar_horario),
+    path('editar-horario/<int:id>/', views.editar_horario),
+    path('deletar-horario/<int:id>/', views.deletar_horario),
 ]
--- 5º semestre(2026-01)/top.43/puma/core/views.py
+++ 5º semestre(2026-01)/top.43/puma/core/views.py
@@ -20,3 +20,25 @@ def cadastrar_horario(request):
         form = HorarioDisponivelForm()
         
     return render(request, 'cadastrar_horario.html', {'form': form})
+
+def editar_horario(request, id):
+    # Busca o horário específico no banco de dados
+    horario = HorarioDisponivel.objects.get(id=id)
+    
+    if request.method == 'POST':
+        # Passa os novos dados (request.POST) e a instância atual para atualizar
+        form = HorarioDisponivelForm(request.POST, instance=horario)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        # Carrega o formulário preenchido com os dados atuais
+        form = HorarioDisponivelForm(instance=horario)
+        
+    return render(request, 'editar_horario.html', {'form': form, 'horario': horario})
+
+def deletar_horario(request, id):
+    # Busca o horário e o apaga diretamente
+    horario = HorarioDisponivel.objects.get(id=id)
+    horario.delete()
+    return redirect('/')
