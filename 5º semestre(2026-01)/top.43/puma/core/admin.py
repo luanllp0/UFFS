d50444a8989f9ff47e020ec1f7cda4559509eb73
Luanllp <luan.llp2005@gmail.com>
2026-06-25 00:37:40 -0300

    remoção de tabelas desnecessárias

--- 5º semestre(2026-01)/top.43/puma/core/admin.py
+++ 5º semestre(2026-01)/top.43/puma/core/admin.py
@@ -1,21 +1,12 @@
 from django.contrib import admin
 from .models import (
-    Perfil,
     Disciplina,
     Turma,
-    Monitoria,
     HorarioDisponivel,
-    Agendamento,
-    Notificacao,
     PadraoSemanal,
 )
 
-
-admin.site.register(Perfil)
 admin.site.register(Disciplina)
 admin.site.register(Turma)
-admin.site.register(Monitoria)
 admin.site.register(HorarioDisponivel)
-admin.site.register(Agendamento)
-admin.site.register(Notificacao)
 admin.site.register(PadraoSemanal)
--- 5º semestre(2026-01)/top.43/puma/core/models.py
+++ 5º semestre(2026-01)/top.43/puma/core/models.py
@@ -2,21 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import timedelta
 
-class Perfil(models.Model):
-    TIPO_USUARIO = [
-        ('ALUNO', 'Aluno'),
-        ('PROFESSOR', 'Professor'),
-    ]
-
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
-    tipo = models.CharField(max_length=20, choices=TIPO_USUARIO)
-    matricula = models.CharField(max_length=20, blank=True, null=True)
-    curso = models.CharField(max_length=100, blank=True, null=True)
-
-    def __str__(self):
-        return f'{self.usuario.username} - {self.tipo}'
-
-
 class Disciplina(models.Model):
     nome = models.CharField(max_length=100)
     codigo = models.CharField(max_length=20, unique=True)
@@ -43,26 +28,6 @@ class Turma(models.Model):
 
     def __str__(self):
         return f'{self.disciplina.codigo} - {self.nome} - {self.semestre}'
-
-
-class Monitoria(models.Model):
-    aluno_monitor = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='monitorias'
-    )
-    turma = models.ForeignKey(
-        Turma,
-        on_delete=models.CASCADE,
-        related_name='monitorias'
-    )
-    ativo = models.BooleanField(default=True)
-
-    class Meta:
-        unique_together = ('aluno_monitor', 'turma')
-
-    def __str__(self):
-        return f'{self.aluno_monitor.username} - {self.turma}'
 
 
 class HorarioDisponivel(models.Model):
@@ -97,49 +62,6 @@ class HorarioDisponivel(models.Model):
     def __str__(self):
         return f'{self.responsavel.username} - {self.turma} - {self.data}'
 
-
-class Agendamento(models.Model):
-    STATUS_AGENDAMENTO = [
-        ('PENDENTE', 'Pendente'),
-        ('CONFIRMADO', 'Confirmado'),
-        ('RECUSADO', 'Recusado'),
-        ('CANCELADO', 'Cancelado'),
-        ('REALIZADO', 'Realizado'),
-    ]
-
-    horario = models.OneToOneField(
-        HorarioDisponivel,
-        on_delete=models.CASCADE,
-        related_name='agendamento'
-    )
-    aluno = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='agendamentos_solicitados'
-    )
-    status = models.CharField(max_length=20, choices=STATUS_AGENDAMENTO)
-    assunto = models.CharField(max_length=150)
-    descricao = models.TextField(blank=True, null=True)
-    criado_em = models.DateTimeField(auto_now_add=True)
-    atualizado_em = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'{self.aluno.username} - {self.horario} - {self.status}'
-
-
-class Notificacao(models.Model):
-    usuario = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='notificacoes'
-    )
-    titulo = models.CharField(max_length=100)
-    mensagem = models.TextField()
-    lida = models.BooleanField(default=False)
-    criada_em = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.usuario.username} - {self.titulo}'
     
 # Adicione a tupla de dias da semana
 DIAS_DA_SEMANA = (
